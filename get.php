e98bc918b613ba94902b5a34a4462b8b06bb26f9
djmin@vip.deyi.com <djmin@vip.deyi.com>
2014-05-08 16:13:17 +0800

    保存数据

---
+++ get.php
@@ -0,0 +1,8 @@
+<?php
+/**
+*    @name:       ./get.php
+*    @author:     unasm<1264310280@qq.com>
+*    @since :     2014-05-06 12:03:17
+*/
+var_dump($_POST);
+?>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$arr = [
+    'b' => "这里是B",
+    'a' => "这里a",
+];
+foreach($arr as $value){
+    var_dump($value);
+    echo "<br/>";
+    echo "<br/>";
+}
+?>
